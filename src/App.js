f2b79c33b0d00d3c4b52469e7a781305ed9ded8a
justinbeharry <jsbeharr@umich.edu>
2018-08-13 18:10:43 -0400

    Create nav bar in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+			<div className='App'>
+				<header className='App-header'>
+					<p id='title'>Beharry Cleaning</p>
+					<p id='hire-button'>Hire Us</p>
 				</header>
-				<p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<div className='content'>
+					<p>Content</p>
+				</div>
 			</div>
 		);
 	}}
