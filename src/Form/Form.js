401f387c51dd787987e8a86276a075f9a13ea1da
justinbeharry <jsbeharr@umich.edu>
2018-08-14 16:45:41 -0400

    Add form styles
    Increase margins between text fields

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Form.css';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-const form = () => {
+const styles = theme => ({
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: 200,
+	}
+});
+
+const form = (props) => {
+
+	const {classes} = props;
+
 	return (
 		<div className='Form'>
 			<div className='card'>
@@ -10,11 +23,13 @@ const form = () => {
 					<p>Fill Out Form</p>
 					<TextField
 						required
+						className={classes.textField}
 						label="First Name"
 						margin="normal"
 					/>
 					<TextField
 						required
+						className={classes.textField}
 						label="Last Name"
 						margin="normal"
 					/>
@@ -24,4 +39,8 @@ const form = () => {
 	);
 };
 
-export default form;
+form.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(form);
