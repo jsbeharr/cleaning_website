bc8ec8c3126a632e7435ca4b2770a09ebc1cc052
justinbeharry <jsbeharr@umich.edu>
2019-05-31 15:09:31 -0400

    Make sure linting works for dockerized app

---
+++ server/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true,
+        "jasmine": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules":true,
+            "arrowFunctions":true,
+            "classes":true,
+            "spread":true,
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "indent": [
+            "error",
+            "tab"
+        ]
+    }
+};
