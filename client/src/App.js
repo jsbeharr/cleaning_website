d288c22e238514cdb5437d74106f705645f60f0a
justinbeharry <jsbeharr@umich.edu>
2019-05-31 22:34:57 -0400

    Implement React Router
    Set up routes for a home,about,service, and contact page

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,48 @@
 import React, { Component } from 'react';
 import Form from './Form/Form';
+import Home from './Home/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<div className='banner'>
-					<header className='banner-header'>
-						<p id='title'>Excellent Cleaning Service</p>
-						<p
-							id='hire-button'
-							onClick={
-								() => {
-									window.scrollTo({
-										top: document.querySelector('form').scrollHeight,
-										behavior: 'smooth'
-									});
+				<Router>
+					<div className='banner'>
+						<header className='banner-header'>
+							<p id='title'>Excellent Cleaning Service</p>
+							<Link to='/'>Home</Link>
+							<Link to='/about'>About Us</Link>
+							<Link to='/service'>Our Service</Link>
+							<Link to='/contact'>Contact</Link>
+							<p
+								id='hire-button'
+								onClick={
+									() => {
+										window.scrollTo({
+											top: document.querySelector('form').scrollHeight,
+											behavior: 'smooth'
+										});
+									}
 								}
-							}
-						>
-							Hire Us
-						</p>
-					</header>
-					<div className='banner-content'>
-						<div className='banner-tagline'>
-							<p>
-								Cleaning Businesses throughout Metro Detroit
+							>
+								Hire Us
 							</p>
-							<p>Have Any Questions?</p>
-							<p>Give Us a Call!</p>
-							<div className='contact'>
-								<p>(734)502-9574</p>
-								<p>excellentcleaningservicemichigan@gmail.com</p>
-							</div>
+						</header>
+						<div className='banner-content'>
+							<Route
+								exact
+								path='/'
+								render={() => <Home/>}
+							/>
 						</div>
 					</div>
-				</div>
-				<div className='form'>
-					<Form />
-				</div>
-			</div >
+					<div className='form'>
+						<Form />
+					</div>
+				</Router>
+			</div>
 		);
 	}
 }
---
+++ client/src/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+	return (
+		<div className='Home'>
+			<div className='banner-content'>
+				<div className='banner-tagline'>
+					<p>
+						Cleaning Businesses throughout Metro Detroit
+					</p>
+					<p>Have Any Questions?</p>
+					<p>Give Us a Call!</p>
+					<div className='contact'>
+						<p>(734)502-9574</p>
+						<p>excellentcleaningservicemichigan@gmail.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
